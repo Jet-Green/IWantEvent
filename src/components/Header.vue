0807c12f3c70ea51a1546272c3e7d70d41202025
Jet-Green <grishadzyin@gmail.com>
2022-06-14 21:48:40 +0400

    add logo and beauty with icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,14 +6,16 @@ let query = ref("");
 
 <template>
   <a-layout>
-    <a-layout-header :style="{ background: 'black', padding: 0 }">
-      <div style="display: flex; justify-content: right">
-        <span class="mdi mdi-24px mdi-map-marker-outline">Глазов</span>
-        <span
-          class="mdi mdi-24px mdi-account-details"
-          style="margin: 0 16px 0 32px"
-        ></span>
+    <a-layout-header style="background: 'black'; padding: 0; display: flex; justify-content: space-between ">
+      <!-- <div style="display: flex; justify-content: space-between"> -->
+      <img :src="logo" style="height: 100%; object-fit: cover; padding: 8px;" />
+      <div style="display: flex; align-items: center">
+        <span class="mdi mdi-map-marker-outline" style="font-size: 40px;"></span>
+        <span>Глазов</span>
+        <a-divider type="vertical" style=" background-color: gray; height: 70%;" />
+        <span class="mdi mdi-account-details" style="margin: 0 16px 0 16px; font-size: 40px; cursor: pointer"></span>
       </div>
+      <!-- </div> -->
     </a-layout-header>
   </a-layout>
 </template>
