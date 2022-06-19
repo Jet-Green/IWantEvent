00de87e25001afaf998022171b24224bb9972847
Grisha Dzyin <grishadzyin@gmail.com>
2022-06-19 15:30:53 +0400

    add info btn

--- src/components/StartPage/CreateEvent.vue
+++ src/components/StartPage/CreateEvent.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref } from "vue";
 
-let checked = ref(false);
+let offerCheckboxChecked = ref(false);
 let eventType = ref(false);
-let offerCheckbox = ref(false);
+let artistName = ref('')
 
 function toggleEvent(val) {
   eventType.value = val;
@@ -13,28 +13,25 @@ function toggleEvent(val) {
   <a-col :xs="24">
     <a-row type="flex" justify="center" align="top" style="margin: 20px 0 0 0">
       <a-col :span="15" :md="15" :xs="22" :sm="18">
-        <span class="text-h6 text-md-h4">Создать мероприятие</span>
+        <a-typography-title>Создать мероприятие</a-typography-title>
         <a-row style="margin: 20px 0 0 0">
           <a-col>
             Открытое мероприятие
             <a-switch v-model:checked="eventType" />
             Закрытое мероприятие
+            <span class="mdi mdi-24px mdi-information-outline"></span>
           </a-col>
         </a-row>
         <div v-if="!eventType">
           <a-row>
             <a-col :span="15" :xs="24" :md="12">
-              <a-input
-                v-model:value="value"
-                size="large"
-                placeholder="Начните вводить имя артиста"
-                style="margin: 20px 0 20px 0"
-              />
+              <a-input v-model:value="artistName" size="large" placeholder="Начните вводить имя артиста"
+                style="margin: 20px 0 20px 0" />
             </a-col>
           </a-row>
           <a-row>
             <a-col>
-              <a-checkbox v-model:checked="checked" style="margin: 0 0 20px 0">
+              <a-checkbox v-model:checked="offerCheckboxChecked" style="margin: 0 0 20px 0">
                 Принять условия оферты
               </a-checkbox>
             </a-col>
