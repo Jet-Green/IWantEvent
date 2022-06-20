da45b69bd1b239b3cdd0bc9fc03cbbc405b1a26b
tooortttt <savelijsutov@gmail.com>
2022-06-20 18:22:37 +0400

    fix mistakes

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
