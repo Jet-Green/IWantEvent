0ccf6927c1d0cd248543c50097b5982980093f50
Jet-Green <grishadzyin@gmail.cm>
2022-06-21 18:16:30 +0400

    Add different baseUrl on different build  modes

--- vite.config.js
+++ vite.config.js
@@ -6,84 +6,88 @@ import { VitePWA } from 'vite-plugin-pwa'
 const path = require('path')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/IWantEvent/',
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'images/apple-touch-icon.png'],
-      manifest: {
-        name: 'Хочу концерт',
-        short_name: 'Хочу концерт',
-        description: 'Description of your app',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'apple-touch-icon.png',
-            sizes: '180x180',
-            type: 'image/png',
-          },
+export default defineConfig(
+  ({ command, mode }) => {
+    let baseUrl = mode == 'development' ? '/' : '/IWantEvent/'
+    return {
+      base: baseUrl,
+      plugins: [
+        vue(),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'images/apple-touch-icon.png'],
+          manifest: {
+            name: 'Хочу концерт',
+            short_name: 'Хочу концерт',
+            description: 'Description of your app',
+            theme_color: '#ffffff',
+            icons: [
+              {
+                src: 'android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: 'apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+              },
 
-          {
-            src: 'mstile-150x150.png',
-            sizes: '150x150',
-            type: 'image/png',
-          },
+              {
+                src: 'mstile-150x150.png',
+                sizes: '150x150',
+                type: 'image/png',
+              },
 
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+              {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+              }
+            ]
           }
-        ]
-      }
-    })
+        })
 
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          'primary-color': '#c0004e',
+      ],
+      define: { 'process.env': {} },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
         },
-        // modifyVars: getThemeVariables({
-        //   dark: true,
-        //   // compact: true,
-        // }),
-        javascriptEnabled: true,
       },
-    },
-  },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
-})
+      css: {
+        preprocessorOptions: {
+          less: {
+            modifyVars: {
+              'primary-color': '#c0004e',
+            },
+            // modifyVars: getThemeVariables({
+            //   dark: true,
+            //   // compact: true,
+            // }),
+            javascriptEnabled: true,
+          },
+        },
+      },
+      /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+      resolve: {
+        extensions: [
+          '.js',
+          '.json',
+          '.jsx',
+          '.mjs',
+          '.ts',
+          '.tsx',
+          '.vue',
+        ]
+      },
+      */
+    }
+  })
