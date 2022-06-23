26af4b6b2d83d1684e75542af8ba7e8ca945214c
Grisha Dzyin <grishadzyin@gmail.com>
2022-06-23 11:24:07 +0400

    add last step

--- src/components/CreateEvent/Steps/OpenEvent/LastStep.vue
+++ src/components/CreateEvent/Steps/OpenEvent/LastStep.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+let ticketCost = ref(1000)
+</script>
 <template>
-    last
+    <a-row type="flex" justify="space-between">
+        <a-col>Тип меоприятия</a-col>
+        <a-col>Открытое</a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+        <a-col>Введите артиста:</a-col>
+        <a-col>БИ-2</a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+        <a-col>Место мероприятия:</a-col>
+        <a-col>Глазов</a-col>
+    </a-row>
+    <a-row type="flex" justify="space-between">
+        <a-col>Стоимость вашего будущего билета:</a-col>
+        <a-col>
+            <a-input v-model:value="ticketCost" size="small" />
+        </a-col>
+    </a-row>
 </template>
