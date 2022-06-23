0028c13e650a9c9bf9cb6a31b9e4d002c511af3b
qbclub <grachevrv@ya.ru>
2022-06-23 08:03:38 +0400

    menu drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,14 @@
 import { ref } from "vue";
 import logo from "../assets/logo.png";
 let query = ref("");
+
+    const visible = ref(false);
+
+    const showDrawer = () => {
+      visible.value = !visible.value;
+    };
+
+   
 </script>
 
 <template>
@@ -12,13 +20,30 @@ let query = ref("");
       <span class="mdi mdi-map-marker-outline" style="font-size: 40px;"></span>
       <span>Глазов</span>
       <a-divider type="vertical" style=" background-color: gray; height: 70%;" />
-      <span class="mdi mdi-account-details" style="margin: 0 16px 0 16px; font-size: 40px; cursor: pointer"></span>
+      <span class="mdi mdi-account-details" @click="showDrawer" style="margin: 0 16px 0 16px; font-size: 40px; cursor: pointer"></span>
     </div>
     <!-- </div> -->
+    <a-drawer
+    bodyStyle=""
+    placement="right"
+    :closable="false"
+    :visible="visible"
+    @close="showDrawer"
+    
+  >
+    <p >Личный кабинет</p>
+    <p>Собираемые</p>
+    <p>Афиша</p>
+     <p>Блог</p>
+    <p>О проекте</p>
+  </a-drawer>
   </a-layout-header>
 </template>
 <style scoped>
 span {
   color: white;
+}
+.ant-drawer-body{
+  background:rgba(0, 0, 0, 0.85) !important;
 }
 </style>
