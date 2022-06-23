9fad0b3da5ea1788b4e62dcc061316507a0b2365
qbclub <grachevrv@ya.ru>
2022-06-23 20:42:31 +0400

    change close1 on small screen

--- src/components/CreateEvent/Steps/CloseEvent/Close1.vue
+++ src/components/CreateEvent/Steps/CloseEvent/Close1.vue
@@ -4,7 +4,7 @@
 <template>
   <a-typography-paragraph
     :ellipsis="ellipsis"
-    content="Выберите форму мероприятия, дату, время и ожидаемое количество гостей."
+    content="Выберите тип мероприятия, дату, время и ожидаемое количество гостей."
     style="margin: 20px 0 20px 0"
   />
 
@@ -12,7 +12,7 @@
     <a-col :xs="8">
       <span style="line-height: 32px">Тип мероприятия:</span>
     </a-col>
-    <a-col :xs="8">
+    <a-col :xs="16">
       <a-select
         placeholder="Свадьба"
         style="width: 120px"
@@ -22,19 +22,19 @@
         <a-select-option value="fest">Свадьба</a-select-option>
       </a-select></a-col
     >
-    <a-col :xs="8"></a-col>
+  
   </a-row>
 
   <a-row class="ma-16">
-    <a-col :xs="8"> <span style="line-height: 32px">Дата:</span> </a-col>
-    <a-col :xs="8">
-      <span style="opacity: 0.6; padding-left: 8px">Hачалo </span>
+    <a-col :xs="24" :sm="8"> <span style="line-height: 32px">Дата:</span> </a-col>
+    <a-col :xs="12" :sm="8">
+      <span style="opacity: 0.6; margin-right: 8px">Hачалo </span>
       <a-space direction="vertical" :size="12">
         <a-date-picker :format="dateFormat" placeholder="2022-06-28" />
       </a-space>
     </a-col>
-    <a-col :xs="8">
-      <span style="opacity: 0.6; padding-left: 8px">Конец </span>
+    <a-col :xs="12" :sm="8">
+      <span style="opacity: 0.6; margin-right: 8px">Конец </span>
       <a-space direction="vertical" :size="12">
         <a-date-picker
           :format="dateFormat"
@@ -43,13 +43,13 @@
     ></a-col>
   </a-row>
   <a-row class="ma-16">
-    <a-col :xs="8"> <span style="line-height: 32px">Время:</span></a-col>
-    <a-col :xs="8">
-      <span style="opacity: 0.6; padding-left: 8px">Hачалo </span>
+    <a-col :xs="24" :sm="8"> <span style="line-height: 32px">Время:</span></a-col>
+    <a-col :xs="12" :sm="8">
+      <span style="opacity: 0.6; margin-right: 8px">Hачалo </span>
       <a-time-picker format="HH:mm" placeholder="17:00" />
     </a-col>
-    <a-col :xs="8">
-      <span style="opacity: 0.6; padding-left: 8px">Конец </span>
+    <a-col :xs="12" :sm="8">
+      <span style="opacity: 0.6; margin-right: 8px">Конец </span>
       <a-time-picker format="HH:mm" placeholder="20:00" />
     </a-col>
   </a-row>
@@ -57,14 +57,13 @@
     <a-col :xs="8"
       ><span style="line-height: 32px">Количество гостей:</span></a-col
     >
-    <a-col :xs="8">
+    <a-col :xs="16">
       <a-input-number type="number"  min="1">
-        <template #addonAfter>
-          <span style="opacity: 0.6">чел. </span>
-        </template>
+      
       </a-input-number>
+       <span style="opacity: 0.6; margin-left: 8px">чел. </span>
     </a-col>
-    <a-col :xs="8"></a-col>
+ 
   </a-row>
 </template>
 <style lang="scss" scoped>
