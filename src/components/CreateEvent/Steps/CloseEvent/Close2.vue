6573cf401974e84f596d4888f2cbbaec9a29a6f4
qbclub <grachevrv@ya.ru>
2022-06-25 01:37:42 +0400

    close2 edit

--- src/components/CreateEvent/Steps/CloseEvent/Close2.vue
+++ src/components/CreateEvent/Steps/CloseEvent/Close2.vue
@@ -1,48 +1,66 @@
-<script setup></script>
+<script setup>
+import EventsPlaceCard from "../../../Cards/EventsPlaceCard.vue";
+</script>
 
 <template>
   <a-row type="flex" justify="space-between" class="section">
-    <a-typography-text style="margin-bottom: 16px">На этом шаге вы можете воспользоваться помощью организатора или
-      создать
+    <a-typography-text style="margin-bottom: 16px"
+      >На этом шаге вы можете воспользоваться помощью организатора или создать
       мероприятие сами.
     </a-typography-text>
     <a-col>
       <a-typography-text>Организатор</a-typography-text>
-      <a-switch style="margin-left: 16px;">
-      </a-switch>
+      <a-switch style="margin-left: 16px"> </a-switch>
     </a-col>
     <a-col :span="12">
       <a-input></a-input>
     </a-col>
     <a-col class="container">
-      <a-button type="primary" shape="round" style="display: flex; align-items: center; margin: 0 0 0 8px">
+      <a-button
+        type="primary"
+        shape="round"
+        style="display: flex; align-items: center; margin: 0 0 0 8px"
+      >
         <span class="mdi mdi-24px mdi-tune-variant"></span>
       </a-button>
-      <span class="mdi mdi-24px mdi-information-outline" style="margin-left: 16px;"></span>
+      <span
+        class="mdi mdi-24px mdi-information-outline"
+        style="margin-left: 16px"
+      ></span>
     </a-col>
   </a-row>
-  <a-typography-text>На втором этапе важно выбрать место проведения и нанять ведущего</a-typography-text>
+  <a-typography-text
+    >На втором этапе важно выбрать место проведения и нанять
+    ведущего</a-typography-text
+  >
   <a-row type="flex" justify="space-between" class="section">
     <a-col>
-      <a-typography-text>
-        Место проведения
-      </a-typography-text>
-      <a-switch style="margin-left: 16px;"></a-switch>
+      <a-typography-text> Место проведения </a-typography-text>
+      <a-switch style="margin-left: 16px"></a-switch>
     </a-col>
     <a-col :span="12">
       <a-input></a-input>
     </a-col>
     <a-col class="container">
-      <a-button type="primary" shape="round" style="display: flex; align-items: center; margin: 0 0 0 8px">
+      <a-button
+        type="primary"
+        shape="round"
+        style="display: flex; align-items: center; margin: 0 0 0 8px"
+      >
         <span class="mdi mdi-24px mdi-tune-variant"></span>
       </a-button>
-      <span class="mdi mdi-24px mdi-information-outline" style="margin-left: 16px;"></span>
+      <span
+        class="mdi mdi-24px mdi-information-outline"
+        style="margin-left: 16px"
+      ></span>
     </a-col>
   </a-row>
 
   <a-row class="cards-container">
-    <a-col v-for="i in 10" :key="i">
-      <div v-if="i != 10" class="card">
+    <div v-for="i in 10" :key="i">
+      <EventsPlaceCard />
+
+      <!-- <div v-if="i != 10" class="card">
         <div class="card-background" :style="{
           'background-image': 'url(https://cloudfront-us-east-1.images.arcpublishing.com/infobae/OP3VXEFN5ZGXPJQTY3PW63XLI4.png)'
         }">
@@ -60,53 +78,79 @@
         <div class="content" style="text-align: center">
           Добавьте свою площадку
         </div>
-      </div>
-    </a-col>
+      </div> -->
+    </div>
+    <div class="add_place">
+      Добавь место
+      <span
+        class="mdi mdi-48px mdi-plus"
+        style="height: 48px; width: 48px"
+      ></span>
+    </div>
   </a-row>
 
   <a-checkbox>Выбрать все места проведения</a-checkbox>
   <a-row type="flex" justify="space-between" class="section">
     <a-col>
-      <a-typography-text>
-        Ведущий
-      </a-typography-text>
-      <a-switch style="margin-left: 16px;"></a-switch>
+      <a-typography-text> Ведущий </a-typography-text>
+      <a-switch style="margin-left: 16px"></a-switch>
     </a-col>
     <a-col :span="12">
       <a-input></a-input>
     </a-col>
     <a-col class="container">
-      <a-button type="primary" shape="round" style="display: flex; align-items: center; margin: 0 0 0 8px">
+      <a-button
+        type="primary"
+        shape="round"
+        style="display: flex; align-items: center; margin: 0 0 0 8px"
+      >
         <span class="mdi mdi-24px mdi-tune-variant"></span>
       </a-button>
-      <span class="mdi mdi-24px mdi-information-outline" style="margin-left: 16px;"></span>
+      <span
+        class="mdi mdi-24px mdi-information-outline"
+        style="margin-left: 16px"
+      ></span>
     </a-col>
   </a-row>
   <a-row class="cards-container">
     <a-col v-for="i in 4" :key="i">
       <div v-if="i != 4" class="card">
-        <div class="card-background" :style="{
-          'background-image': 'url(https://www.soyuz.ru/public/uploads/files/3/6977740/20170323104022e366171b00.jpg)'
-        }">
-        </div>
+        <div
+          class="card-background"
+          :style="{
+            'background-image':
+              'url(https://www.soyuz.ru/public/uploads/files/3/6977740/20170323104022e366171b00.jpg)',
+          }"
+        ></div>
         <div class="content">
-          <a-typography-title :level="5">Адександр Золототарёв</a-typography-title>
+          <a-typography-title :level="5"
+            >Адександр Золототарёв</a-typography-title
+          >
         </div>
       </div>
       <div v-else class="card last-card">
-        <div class="card-background"
-          style="display: flex; justify-content: center; align-items: center; opacity: 1 !impotant;">
-          <span class="mdi mdi-48px mdi-plus" style="height: 48px; width: 48px"></span>
+        <div
+          class="card-background"
+          style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            opacity: 1 !impotant;
+          "
+        >
+          <span
+            class="mdi mdi-48px mdi-plus"
+            style="height: 48px; width: 48px"
+          ></span>
         </div>
-        <div class="content" style="text-align: center">
-          Добавьте ведущего
-        </div>
+        <div class="content" style="text-align: center">Добавьте ведущего</div>
       </div>
     </a-col>
   </a-row>
   <a-checkbox style="margin: 8px 0 8px 0">Выбрать всех ведущих</a-checkbox>
 </template>
-<style scoped>
+
+<style lang="scss" scoped>
 .container {
   display: flex;
   flex-direction: row;
@@ -115,6 +159,23 @@
 .cards-container {
   display: flex;
   flex-direction: row;
+}
+.add_place {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 8px;
+  width: 120px;
+  aspect-ratio: 1;
+  box-sizing: border-box;
+
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+   &:active {
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.3);
+    padding:2px;
+    font-size:98%;
+  }
 }
 
 .card {
