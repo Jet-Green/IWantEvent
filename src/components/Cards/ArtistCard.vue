e5445cc2bb6a334d93f401fec07d17a3fa2f3e59
Grisha Dzyin <grishadzyin@gmail.com>
2022-06-25 08:54:24 +0400

    add 3 step closed event

---
+++ src/components/Cards/ArtistCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props = defineProps(['artistCardInfoProps'])
+let artistCardInfo = props.artistCardInfoProps
+</script>
+<template>
+    <div class="artist_card">
+        <div class="for_img"> <img :src="artistCardInfo.image" alt="" /></div>
+
+        <div class="for_text">
+            {{ artistCardInfo.artistName }}
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.artist_card {
+    margin: 8px;
+    width: 120px;
+    aspect-ratio: 1;
+    box-sizing: border-box;
+    font-size: 10px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.5);
+    border-radius: 8px;
+
+    .for_img {
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 90%;
+        border-radius: 8px 8px 0 0;
+
+        img {
+            height: 100%;
+        }
+    }
+
+    .for_text {
+        height: 10%;
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-transform: uppercase;
+    }
+}
+</style>
--- src/components/CreateEvent/Steps/CloseEvent/Close3.vue
+++ src/components/CreateEvent/Steps/CloseEvent/Close3.vue
@@ -1,9 +1,104 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+
+import ArtistCard from '../../../../components/Cards/ArtistCard.vue'
+
+let selectedArtists = reactive([{
+  number: 1,
+  artistName: 'Руки Вверх',
+  image: 'https://i.scdn.co/image/ab67616d0000b2738d8519ff4ee9a3d773bcecd9'
+}, {
+  number: 2,
+  artistName: 'Руки Вниз(текст переполняет)',
+  image: 'https://i.scdn.co/image/ab67616d0000b2738d8519ff4ee9a3d773bcecd9'
+}])
+
+function addArtist() {
+  let number = selectedArtists[selectedArtists.length - 1].number
+  selectedArtists.push({
+    number: number + 1,
+    artistName: '',
+    image: ''
+  })
+}
+</script>
 
 <template>
-  <a-typography-paragraph
-    :ellipsis="ellipsis"
-    content=" Третий этап"
-    style="margin: 20px 0 20px 0"
-  />
+  <a-row class="section">
+    <a-typography-text>Теперь важно определиться с артистами</a-typography-text>
+  </a-row>
+  <div v-for="(card, index) in selectedArtists" :key="index" class="section">
+    <a-row type="flex" justify="space-between">
+      <a-col>
+        <a-typography-text>Артист №{{ card.number }}</a-typography-text>
+      </a-col>
+      <a-col :span="12">
+        <a-input placeholder="Начните водить имя" v-model:value="selectedArtists[card.number - 1].artistName"></a-input>
+      </a-col>
+      <a-col style="display: flex; flex-direction: row;">
+        <a-button type="primary" shape="round" style="display: flex; align-items: center; margin: 0 0 0 8px">
+          <span class="mdi mdi-24px mdi-tune-variant"></span>
+        </a-button>
+        <span class="mdi mdi-24px mdi-information-outline" style="margin-left: 16px"></span>
+      </a-col>
+    </a-row>
+    <ArtistCard :artistCardInfoProps="card" />
+    <div v-if="index == selectedArtists.length - 1" class="section">
+      <a-button type="link" @click="addArtist">
+        Добавьте ещё +
+      </a-button>
+    </div>
+  </div>
+  <div class="card last-card">
+    <div class="card-background" style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            opacity: 1 !impotant;
+          ">
+      <span class="mdi mdi-48px mdi-plus" style="height: 48px; width: 48px"></span>
+    </div>
+    <div class="content" style="text-align: center">Добавьте своего артиста</div>
+  </div>
 </template>
+<style lang="scss" scoped>
+.section {
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+
+.card {
+  position: relative;
+
+  width: 120px;
+  aspect-ratio: 1;
+
+  margin: 0 8px 8px 0;
+}
+
+.last-card {
+  display: flex;
+  background: rgb(240, 240, 240);
+}
+
+.card .content {
+  position: relative;
+  z-index: 1;
+}
+
+.content {
+  margin: 8px;
+}
+
+.card-background {
+  z-index: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-size: cover;
+  opacity: 0.75;
+  border-radius: 8px;
+}
+</style>
