8e423d4a7fd6a8068f24e2f3bb5d616340e504f3
Grisha Dzyin <grishadzyin@gmail.com>
2022-06-26 20:30:08 +0400

    add first test

---
+++ src/__tests__/unit/Close4.test.js
@@ -0,0 +1,10 @@
+import { describe, it, } from "vitest"
+import { render } from "@testing-library/vue"
+
+import Close4 from '../../components/CreateEvent/Steps/CloseEvent/Close4.vue'
+
+describe('Close4 tests', () => {
+    it('should render', async () => {
+        const wrapper = render(Close4)
+    })
+})
