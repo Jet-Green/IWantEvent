59a468afa5240ebfd3f520f49285068aca616e6a
warcen11 <warcen2006@gmail.com>
2022-06-27 18:26:02 +0400

    workingAtThis

--- src/components/CreateEvent/Steps/CloseEvent/Close5.vue
+++ src/components/CreateEvent/Steps/CloseEvent/Close5.vue
@@ -2,8 +2,8 @@
 
 <template>
   <a-typography-paragraph
-    :ellipsis="ellipsis"
-    content=" Пятый этап"
+    content="Поздравляем, вы почти создали своё закрытое мероприятие!"
     style="margin: 20px 0 20px 0"
   />
+  <a-typography-paragraph content="Теперь проверяем, что получилось."/>
 </template>
