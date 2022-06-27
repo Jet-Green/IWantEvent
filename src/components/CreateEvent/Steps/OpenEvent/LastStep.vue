ed8b765f60725e0f2d9c1941bf140080d4ceb218
warcen11 <warcen2006@gmail.com>
2022-06-27 17:17:02 +0400

    lastStep

--- src/components/CreateEvent/Steps/OpenEvent/LastStep.vue
+++ src/components/CreateEvent/Steps/OpenEvent/LastStep.vue
@@ -1,24 +1,30 @@
 <script setup>
 import { ref } from 'vue'
-let ticketCost = ref(1000)
+let ticketCost = ref(null)
+let place = ref('')
+let artist = ref('')
 </script>
 <template>
   <a-row style="margin-top: 20px;">
     <a-col :span="14">Тип мероприятия</a-col>
-    <a-col :span="7">Открытое</a-col>
+    <a-col :span="7" style="margin-left: 10px">Открытое</a-col>
   </a-row>
   <a-row>
     <a-col :span="14">Введите артиста:</a-col>
-    <a-col :span="7">БИ-2</a-col>
+    <a-col :span="7">
+    <a-input v-model:value="artist" :bordered="false" placeholder="БИ-2"/>
+    </a-col>
   </a-row>
   <a-row>
     <a-col :span="14">Место мероприятия:</a-col>
-    <a-col :span="7">Глазов</a-col>
+    <a-col :span="7">
+    <a-input v-model:value="place" :bordered="false" placeholder="Глазов" />
+    </a-col>
   </a-row>
   <a-row>
-    <a-col :span="24" :sm="14">Стоимость вашего будущего билета:</a-col>
+    <a-col :span="24" :sm="14" style="margin-left: 3px">Стоимость вашего будущего билета:</a-col>
     <a-col :span="7">
-      <a-input v-model:value="ticketCost" size="small" />
+      <a-input v-model:value="ticketCost" type="number" size="small" placeholder="1000"/>
     </a-col>
   </a-row>
   <a-row style="margin: 16px 0 16px 0">
