0b6b306d24350ef74feb9c9c48106d4c577f254c
warcen11 <warcen2006@gmail.com>
2022-06-28 15:08:28 +0400

    nothingAtAll

--- src/components/CreateEvent/Steps/CloseEvent/Close5.vue
+++ src/components/CreateEvent/Steps/CloseEvent/Close5.vue
@@ -5,5 +5,18 @@
     content="Поздравляем, вы почти создали своё закрытое мероприятие!"
     style="margin: 20px 0 20px 0"
   />
-  <a-typography-paragraph content="Теперь проверяем, что получилось."/>
+  <a-typography-paragraph content="Теперь проверяем, что получилось."
+    style="margin: 20px 0 20px 0"
+  />
+  <a-row>
+    <a-col>
+      Шаг 1
+    </a-col>
+    <a-col>
+
+    </a-col>
+    <a-col>
+      
+    </a-col>
+  </a-row>
 </template>
