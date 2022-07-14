7ce2c2e5abe618963b7e993ce27d4f7aa2300c6a
regtyit <titov54275@gmail.com>
2022-07-14 19:47:51 +0400

    EventAdditionalOption fix

--- src/components/Cards/EventAdditionalOption.vue
+++ src/components/Cards/EventAdditionalOption.vue
@@ -1,38 +1,50 @@
 <script setup>
-// to use required image you need add to this component v-bind:image"'one'" or "'two'" or "'three'" (notice that you must send string)
-import { reactive, ref } from "vue";
-let props = defineProps({ image: String });
-let image = props.image;
-let AdditionalOptions = reactive({
-  one: "https://new-variant.ru/wp-content/uploads/2020/02/1-56.jpg",
-  two:
-    "https://kkaravaev.com/wa-data/public/shop/products/69/26/12669/images/20932/20932.1200.jpg",
-  three: "https://mediasole.ru/data/images/338/338417/2.jpg",
-});
+// to use required image you need add to this component v-bind:image"'one'" or "'two'" or "'three'" (notice that you must send string)(but not now)
+import { ref } from "vue";
+let image = ref(
+  "https://kkaravaev.com/wa-data/public/shop/products/69/26/12669/images/20932/20932.1200.jpg"
+);
+let text = ref("Мерч + 500 руб");
 </script>
 <template>
-  <div class="for_img" v-if="image === 'one'">
-    <img :src="AdditionalOptions.one" alt="" />
-  </div>
-  <div class="for_img" v-else-if="image === 'two'">
-    <img :src="AdditionalOptions.two" alt="" />
-  </div>
-  <div class="for_img" v-else>
-    <img :src="AdditionalOptions.three" alt="" />
+  <div class="place_card">
+    <div class="for_img">
+      <img :src="image" alt="no internet(" />
+    </div>
+    <div class="for_text">
+      {{ text }}
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.for_img {
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-   height: 20vh;
-    width: 20vw;
-  border-radius: 8px 8px 0 0;
-  img {
-    height: 20vh;
-    width: 20vw;
+.place_card {
+  margin: 8px;
+  width: 160px;
+  height: 160px;
+  aspect-ratio: 1;
+  box-sizing: border-box;
+  font-size: 12px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+  .for_img {
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    border-radius: 8px 8px 0 0;
+    img {
+      height: 100%;
+      width: 100%;
+    }
+  }
+  .for_text {
+    height: 25%;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
   }
 }
 </style>
